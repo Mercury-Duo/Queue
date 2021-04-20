47152939165217a6c544b6c03f5fccea1c196261
铎 <@DESKTOP-733MSEH>
2021-04-20 19:38:58 +0800

    NULL

--- 队列/队列.cpp
+++ 队列/队列.cpp
@@ -9,24 +9,44 @@ struct Queue {
 	struct Node* frontNode;
 	struct Node* tailNode;
 };
-struct Node* CreatNode(int data) {
+struct Node* CreatNode(int data) {//初始化节点
 	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
 	newNode->data = data;
 	newNode->next = NULL;
 	return newNode;
 }
-struct Queue* CreatQueue(int data) {
+struct Queue* CreatQueue() {//新建队列
 	struct Queue* myQueue = (struct Queue*)malloc(sizeof(struct Queue));
-	//struct Node* node1 = CreatNode(data);
 	myQueue->frontNode = myQueue->tailNode = NULL;
 	myQueue->size = 0;
 	return myQueue;
 }
-int empty(struct Queue* myQueue) {
+int empty(struct Queue* myQueue) {//判断是否为空
 	if (myQueue->size == 0)return 0;
 	return 1;
 }
-
-
+void push(struct Queue* myQueue, int data) {
+	struct Node* node1 = CreatNode(data);
+	if (myQueue->size == 0) {
+		myQueue->frontNode = myQueue->tailNode = node1;
+	}
+	else {
+		myQueue->tailNode->next = node1;
+		myQueue->tailNode = node1;
+	}
+	myQueue->size++;
+}
+void pop(struct Queue* myQueue) {
+	if (myQueue->size == 0) {
+		printf("队为空");
+		return;
+	}
+	else {
+		struct Node* nextNode = myQueue->frontNode->next;
+		free(myQueue->frontNode);
+		myQueue->frontNode = nextNode;
+		myQueue->size--;
+	}
+}
 
 int main(void){}
